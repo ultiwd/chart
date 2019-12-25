01d9116265534111f1eca27b7a63927c3455b2ba
max <ultiwd@gmail.com>
2019-12-25 10:30:02 -0500

    output path

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  output: {
+    path: __dirname + '/public',
+  },
   module: {
     rules: [
       {
