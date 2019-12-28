38f7b4c47995e6af057e7c2f6b2d498eebe2eddb
max <ultiwd@gmail.com>
2019-12-28 16:46:42 -0500

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   output: {
-    path: __dirname + '/public',
+    path: __dirname + "/public"
   },
   module: {
     rules: [
@@ -13,7 +13,17 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: [["@babel/preset-env"]],
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    "browsers": ["> 1%", "last 2 versions", "Android >= 3.2", "not ie <= 8"]
+                  },
+                  useBuiltIns: true
+                }
+              ]
+            ],
             plugins: [
               ["@babel/plugin-proposal-class-properties", { loose: false }],
               "@babel/plugin-syntax-dynamic-import",
@@ -26,7 +36,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: "./index.html"
     })
   ]
 };
