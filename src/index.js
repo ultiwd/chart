959f76f40655d3b6f25cda001c51c49d515844e6
max <ultiwd@gmail.com>
2019-12-28 15:35:21 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 // Packages
-import {Gitlab} from 'gitlab/dist/index.browser'
-// const db = require("./db");
-
-// Modules
-// const getTemplate = require("./template.js");
-const { token, host } = require("./config.js");
 // Helpers
+import { getDates, getData, convertData, transformData } from "./helpers";
 
-import {
-  getDates,
-  getData,
-  convertData,
-  transformData
-} from './helpers'
-
-const api = new Gitlab({
-  host,
-  token,
-});
+const apiUrl = "https://damp-reaches-06511.herokuapp.com/";
 
 const currentDay = new Date().getUTCDate();
 const currentMonth = new Date().getUTCMonth() + 1;
@@ -33,20 +18,13 @@ const setLabel = label => {
 const doneIssueLabel = "Done Dev";
 
 const getContent = async () => {
-  const [milestone] = await api.ProjectMilestones.all(50, {
-    perPage: 1,
-    maxPages: 1,
-    state: "active",
-  });
+  const [milestone] = await fetch(`${apiUrl}/milestone`).then(r => r.json());
 
-  const issues = await api.Issues.all({
-    projectId: 50,
-    milestone: milestone.title,
-    state: "opened",
-    labels: teamLabel
-  });
+  const issues = await fetch(`${apiUrl}/issues`).then(r => r.json());
 
-  const labels = (await api.Labels.all(50)).filter(e => e.name === teamLabel);
+  const labels = (await fetch(`${apiUrl}/labels`).then(r => r.json())).filter(
+    e => e.name === teamLabel
+  );
 
   const {
     startYear,
@@ -81,6 +59,12 @@ const getContent = async () => {
     )
   ]);
 
+  const getIssuesStatistics = async (year, month, day) =>
+    fetch(`${apiUrl}/issues_statistics`, {
+      method: "POST",
+      body: JSON.stringify({ year, month, day })
+    }).then(r => r.json());
+
   const issuesCount = Promise.all(
     monthData
       .filter(
@@ -89,17 +73,7 @@ const getContent = async () => {
           e.month === currentMonth &&
           e.year === currentYear
       )
-      .map((e, i, arr) =>
-        api.IssuesStatistics.all({
-          projectId: 50,
-          groupId: 17,
-          state: "opened",
-          updated_before: new Date(
-            `${e.year} ${e.month} ${e.day[0]} 10:00`
-          ).toISOString(),
-          labels: "Done Dev, Team Jedi"
-        })
-      )
+      .map(e => getIssuesStatistics(e.year, e.month, e.day[0]))
   ).then(v => v.map(e => issues.length - e.statistics.counts.opened));
   return { issues, idealMonthData, monthData, labels, milestone, issuesCount };
 };
@@ -125,99 +99,100 @@ const getContent = async () => {
     issuesArr: issuesArr
   };
 })();
-const {
-  issuesLength,
-mappedDates,
-days,
-color,
-issuesArr
-} = window.config;
-document.querySelector('select').addEventListener('change', (e) => {
-fetch('/update',{
-  method: 'POST',
-  body: JSON.stringify({label: e.target.value}),
-  headers: {
-      'Content-Type': 'application/json'
-    },
-}).then(r => r.json()).then(r => {
+const { issuesLength, mappedDates, days, color, issuesArr } = window.config;
+document.querySelector("select").addEventListener("change", e => {
+  fetch("/update", {
+    method: "POST",
+    body: JSON.stringify({ label: e.target.value }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(r => r.json())
+    .then(r => {
+      chart.destroy();
+      chart = createChart(r.issuesArr, r.mappedDates, r.issuesLength, r.color);
+      chart.data.label = r.label;
+      chart.update();
+    });
+});
 
-  chart.destroy()
-  chart = createChart(r.issuesArr, r.mappedDates, r.issuesLength, r.color)
-  chart.data.label = r.label
-  chart.update()
-})
-})
-
-const canvas = document.getElementById('myChart')
-canvas.width = 1800
-canvas.height = 800
+const canvas = document.getElementById("myChart");
+canvas.width = 1800;
+canvas.height = 800;
 // Chart.defaults.global.elements.point.pointStyle = 'dash'
-let i = 0
-const ctx = canvas.getContext('2d');
-Chart.defaults.global.plugins.datalabels.formatter = (value) => value.y
-
+let i = 0;
+const ctx = canvas.getContext("2d");
+Chart.defaults.global.plugins.datalabels.formatter = value => value.y;
 
 function createChart(issuesArr, mappedDates, issuesLength, color) {
-return new Chart(ctx, {
-  plugins: [ChartDataLabels],
-  type: 'line',
-  data: {
-      datasets: [{
+  return new Chart(ctx, {
+    plugins: [ChartDataLabels],
+    type: "line",
+    data: {
+      datasets: [
+        {
           datalabels: {
-              align: 'top',
-              font: {
-                  size: 20,
-                  weight: 'bold',
-              },
-              labels: {
-                  value: {},
-                  title: {
-                      // color: 'blue'
-                  }
+            align: "top",
+            font: {
+              size: 20,
+              weight: "bold"
+            },
+            labels: {
+              value: {},
+              title: {
+                // color: 'blue'
               }
+            }
           },
-          label: 'Sprint Burndown',
+          label: "Sprint Burndown",
           data: issuesArr,
           lineTension: 0.2,
-          backgroundColor: 'rgba(0, 0, 0, 0)',
-          borderColor:  
-              // '#03a9f4'
-              color
-      },{
+          backgroundColor: "rgba(0, 0, 0, 0)",
+          borderColor:
+            // '#03a9f4'
+            color
+        },
+        {
           datalabels: {
-              display: false
+            display: false
           },
-          label: 'Ideal Burndown',
+          label: "Ideal Burndown",
           data: mappedDates,
-          backgroundColor: 'rgba(0, 0, 0, 0)' ,
+          backgroundColor: "rgba(0, 0, 0, 0)",
           borderWidth: 1
-      }],
+        }
+      ],
       labels: days
-  },
+    },
 
-  options: {
+    options: {
       responsive: false,
       animation: {
-          easing: 'linear',
-      },    
+        easing: "linear"
+      },
       scales: {
-          yAxes: [{
-              gridLines: {
-                  display:false
-              },
-              ticks: {
-                  max: issuesLength,
-                  min: 0
-              }
-          }],
-          xAxes: [{
-              gridLines: {
-                  display:false
-              }
-          }]
+        yAxes: [
+          {
+            gridLines: {
+              display: false
+            },
+            ticks: {
+              max: issuesLength,
+              min: 0
+            }
+          }
+        ],
+        xAxes: [
+          {
+            gridLines: {
+              display: false
+            }
+          }
+        ]
       }
-  }
-});        
+    }
+  });
 }
 
-let chart = createChart([issuesArr], [mappedDates], issuesLength, color)
+let chart = createChart([issuesArr], [mappedDates], issuesLength, color);
